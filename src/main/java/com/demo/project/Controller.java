9848d250225260e7ac2d46af8322911bac92d7a6
vitoudev9 <vitoudev9@gmail.com>
2024-12-18 21:41:19 +0700

    commit

---
+++ src/main/java/com/demo/project/Controller.java
@@ -0,0 +1,13 @@
+package com.demo.project;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping("/")
+    public String home() {
+        return "This is homepage";
+    }
+}
